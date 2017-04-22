70c9e02d38d05e1d56428afa2fe982a3aacde585
Seurimas <nicholasanderson5@gmail.com>
2017-04-22 17:31:33 -0500

    Adding basic AI.

---
+++ Assets/enemies/DrakeAI.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrakeAI : MonoBehaviour {
+	public float fireRange = 4;
+	public float speed = 6;
+	public GameObject prefab;
+	public GameObject target;
+	Rigidbody2D rb2d;
+	Animator animator;
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D> ();
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (target != null && target.activeInHierarchy) {
+			attack (target);
+		} else {
+			animator.Play ("DrakeStand");
+			rb2d.velocity = Vector2.zero;
+		}
+	}
+
+	void attack(GameObject other) {
+		Vector2 movement = other.transform.position - transform.position;
+		if (movement.magnitude > fireRange) {
+			rb2d.velocity = movement.normalized * speed;
+			animator.Play ("DrakeWalk");
+			if (rb2d.velocity.x < 0) {
+				GetComponent<SpriteRenderer> ().flipX = true;
+			} else if (rb2d.velocity.x > 0) {
+				GetComponent<SpriteRenderer> ().flipX = false;
+			}
+		} else {
+			rb2d.velocity = Vector2.zero;
+			animator.Play ("DrakeAttack");
+		}
+	}
+
+	public void Aggro(GameObject other) {
+		target = other;
+	}
+
+	public void Fire() {
+		Vector3 targetVector = target.transform.position - transform.position;
+		float angle = Mathf.Atan2 (targetVector.y, targetVector.x) * Mathf.Rad2Deg;
+		Debug.Log (angle);
+		GameObject fireball = Instantiate (prefab, transform.position, Quaternion.AngleAxis (angle, Vector3.forward));
+		fireball.GetComponent<Rigidbody2D> ().velocity = targetVector.normalized * 16;
+	}
+}
---
+++ Assets/enemies/DrakeSensor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrakeSensor : MonoBehaviour {
+	DrakeAI ai;
+	// Use this for initialization
+	void Start () {
+		ai = gameObject.GetComponentInParent<DrakeAI> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.GetComponent<playerMoveRB2D> ()) {
+			ai.Aggro (other.gameObject);
+		}
+	}
+}
