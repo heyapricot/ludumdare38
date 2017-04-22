1f0bf62bda21097b46deab7b4bafe1cbf369d4c3
Alvaro Sanchez <heyalvaro@gmail.com>
2017-04-22 17:47:37 -0500

    Added ability to draw sword with spacebar. When pressing space again, the sword deactivates

---
+++ Assets/scripts/SwingingWeapon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwingingWeapon : MonoBehaviour {
+    public float swingAngle = 180;
+    public float targetAngle;
+    public float duration;
+    private float progress = 0;
+    // Use this for initialization
+    void Start () {
+        transform.RotateAround(transform.parent.position, Vector3.back, getAngleAtTime(0));
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float previousAngle = getAngleAtTime(progress);
+        progress += Time.deltaTime;
+        float percentThrough = progress / duration;
+        float currentAngle = getAngleAtTime(progress);
+        transform.RotateAround(transform.parent.position, Vector3.back, (currentAngle - previousAngle));
+        if (percentThrough >= 1)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    private float getAngleAtTime(float t)
+    {
+        return Mathf.LerpAngle(targetAngle - swingAngle / 2, targetAngle + swingAngle / 2, t / duration);
+    }
+}
---
+++ Assets/scripts/playerSwingSword.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSwingSword : MonoBehaviour {
+
+    public GameObject swingingWeaponPrefab;
+    public float swingDuration;
+    private GameObject swingObject;
+    Rigidbody2D rb2d;
+    Rigidbody2D swordRb2d;
+    Vector3 playerPosition;
+    bool isCreated = false;
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        
+        //Create sword and disable it
+        swingObject = Instantiate(swingingWeaponPrefab, rb2d.position, Quaternion.AngleAxis(90, new Vector3(0, 0, 1)));
+        swordRb2d = swingObject.GetComponent<Rigidbody2D>();
+        swingObject.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        trigger_swing();
+	}
+
+    private void trigger_swing()
+    {
+        if (Input.GetKeyDown("space"))
+            drawWeapon();
+    }
+
+    private void drawWeapon()
+    {
+        swingObject.transform.position = new Vector2(-0.5f, 0) + rb2d.position;
+        if (!isCreated)
+        {
+            swingObject.SetActive(true);
+            isCreated = true;
+        }
+        else
+        {
+            swingObject.SetActive(false);
+            isCreated = false;
+        }
+
+    }
+
+    private void swing()
+    {
+        
+    }
+
+    IEnumerator delay()
+    {
+        yield return new WaitForSeconds(swingDuration);
+    }
+}
