f8595b59c647ae9f79da4ae06802f6bc5d0c76a5
Seurimas <nicholasanderson5@gmail.com>
2017-04-22 15:36:58 -0500

    Adding drake with firebreathing.

---
+++ Assets/Dissipate.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dissipate : MonoBehaviour {
+	public float lifetime = 2f;
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, lifetime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
