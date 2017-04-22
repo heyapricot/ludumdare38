c8707b0f1a45e97abbfabf39bd0794f4f117574f
Alvaro Sanchez <heyalvaro@gmail.com>
2017-04-22 09:57:36 -0500

    Created dummy map and dummy player to test for movement with WASD

---
+++ Assets/scripts/player_move.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_move : MonoBehaviour {
+
+    Vector3 pos;                                // For movement
+    float speed = 96.0f;                         // Speed of movement
+
+    void Start()
+    {
+        pos = transform.position;          // Take the initial position
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A) && transform.position == pos)
+        {        // Left
+            pos += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D) && transform.position == pos)
+        {        // Right
+            pos += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.W) && transform.position == pos)
+        {        // Up
+            pos += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.S) && transform.position == pos)
+        {        // Down
+            pos += Vector3.down;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, pos, Time.deltaTime * speed);    // Move there
+    }
+}
