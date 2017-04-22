b4346f2e27b1a984662eb27886f6a5958a4e8aaa
Alvaro Sanchez <heyalvaro@gmail.com>
2017-04-22 11:45:05 -0500

    Implemented movement using rigidBody2D and removed restriction where current position == final position to allow movement

---
+++ Assets/scripts/playerMoveRB2D.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMoveRB2D : MonoBehaviour
+{
+
+    Vector3 pos;                                // For movement
+    public float speed = 96.0f;                         // Speed of movement
+    Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        pos = rb2d.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {        // Left
+            pos += Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {        // Right
+            pos += Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {        // Up
+            pos += Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {        // Down
+            pos += Vector3.down;
+        }
+
+        rb2d.MovePosition(Vector3.MoveTowards(rb2d.position, pos, Time.deltaTime * speed));    // Move there
+    }
+}
