c41c635747047d788747fe987295eeba478ad810
Seurimas <nicholasanderson5@gmail.com>
2017-04-23 13:15:40 -0500

    Adding colored drakes.

--- Assets/enemies/DrakeAI.cs
+++ Assets/enemies/DrakeAI.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class DrakeAI : MonoBehaviour {
+	public enum DrakeColor {
+		RED,
+		BLUE,
+		GREEN,
+		BLACK
+	};
+	public DrakeColor myColor = DrakeColor.RED; 
 	public float fireRange = 4;
 	public float speed = 6;
 	public GameObject prefab;
 	public GameObject target;
+	bool shooting = false;
 	Rigidbody2D rb2d;
 	Animator animator;
 	// Use this for initialization
@@ -20,16 +28,16 @@ public class DrakeAI : MonoBehaviour {
 		if (target != null && target.activeInHierarchy) {
 			attack (target);
 		} else {
-			animator.Play ("DrakeStand");
+			animator.Play (GetColor() + "Stand");
 			rb2d.velocity = Vector2.zero;
 		}
 	}
 
 	void attack(GameObject other) {
 		Vector2 movement = other.transform.position - transform.position;
-		if (movement.magnitude > fireRange) {
+		if (movement.magnitude > fireRange && !shooting) {
 			rb2d.velocity = movement.normalized * speed;
-			animator.Play ("DrakeWalk");
+			animator.Play (GetColor() + "Walk");
 			if (rb2d.velocity.x < 0) {
 				GetComponent<SpriteRenderer> ().flipX = true;
 			} else if (rb2d.velocity.x > 0) {
@@ -37,12 +45,28 @@ public class DrakeAI : MonoBehaviour {
 			}
 		} else {
 			rb2d.velocity = Vector2.zero;
-			animator.Play ("DrakeAttack");
+			animator.Play (GetColor() + "Attack");
+			shooting = true;
 		}
 	}
 
 	public void Aggro(GameObject other) {
 		target = other;
+	}
+
+	public string GetColor() {
+		switch (myColor) {
+		case DrakeColor.RED:
+			return "Red";
+		case DrakeColor.BLACK:
+			return "Black";
+		case DrakeColor.BLUE:
+			return "Blue";
+		case DrakeColor.GREEN:
+			return "Green";
+		default:
+			return "Red";
+		}
 	}
 
 	public void Fire() {
@@ -51,5 +75,6 @@ public class DrakeAI : MonoBehaviour {
 		Debug.Log (angle);
 		GameObject fireball = Instantiate (prefab, transform.position, Quaternion.AngleAxis (angle, Vector3.forward));
 		fireball.GetComponent<Rigidbody2D> ().velocity = targetVector.normalized * 16;
+		shooting = false;
 	}
 }
