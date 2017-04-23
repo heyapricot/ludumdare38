812006f5da4f015c290ebcdbe187e2aba11e8e90
Alvaro Sanchez <heyalvaro@gmail.com>
2017-04-23 07:59:09 -0500

    Created enable/disable of weapon appearance by pressing spacebar. Weapon is a child of the player. Also rearranged the scripts fodler

---
+++ Assets/scripts/player/playerSwingWeapon.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerSwingWeapon : MonoBehaviour {
+    GameObject weapon;
+    Vector3 playerPosition;
+    bool weaponOut = false;
+	// Use this for initialization
+	void Start () {
+        
+        //Weapon is the first child of player
+        weapon = this.gameObject.transform.GetChild(0).gameObject;
+        //Hide weapon
+        weapon.SetActive(false);
+
+        //Get player
+        playerPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void trigger_swing()
+    {
+        if (Input.GetKeyDown("space"))
+            drawWeapon();
+    }
+
+    private void drawWeapon()
+    {
+        weapon.transform.position = new Vector3(-0.5f, 0) + playerPosition;
+        if (!weaponOut)
+        {
+            weapon.SetActive(true);
+            weaponOut = true;
+        }
+        else
+        {
+            weapon.SetActive(false);
+            weaponOut = false;
+        }
+
+    }
+}
