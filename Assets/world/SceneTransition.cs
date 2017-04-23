3e7d718cc04d03a4606cb551c1f4c2c8f21924de
Seurimas <nicholasanderson5@gmail.com>
2017-04-23 16:38:07 -0500

    Adding an old man.

--- Assets/world/SceneTransition.cs
+++ Assets/world/SceneTransition.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneTransition : MonoBehaviour {
+	static GameObject lastTransition;
 	static bool inTransition = false;
 	public GameObject myWorld;
 	public GameObject newWorld;
@@ -17,14 +18,21 @@ public class SceneTransition : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Entering.");
 		if (!inTransition && other.GetComponent<playerMoveRB2D> () != null) {
+			Debug.Log ("Transitioning.");
 			inTransition = true;
+			lastTransition = gameObject;
 			myWorld.SetActive (false);
 			newWorld.SetActive (true);
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D other) {
-		inTransition = false;
+		Debug.Log ("Leaving.");
+		if (other.GetComponent<playerMoveRB2D> () != null) {
+			Debug.Log ("Exiting.");
+			inTransition = false;
+		}
 	}
 }
