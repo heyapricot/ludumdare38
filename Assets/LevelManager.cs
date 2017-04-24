b1a6b7a03dc97f047d2d159eb6656c1f1342a309
Alvaro Sanchez <heyalvaro@gmail.com>
2017-04-24 17:41:38 -0500

    Game goes back to menu when player is dead after 3 seconds

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour {
-
+    GameObject player;
 	// Use this for initialization
 	void Start () {
-		
+        player = GameObject.Find("player");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        isGameOver(player);
 	}
 
     public void LoadLevel(string name)
@@ -20,4 +20,24 @@ public class LevelManager : MonoBehaviour {
         Debug.Log("New Level load: " + name);
         SceneManager.LoadScene(name);
     }
+
+    public bool isGameOver(GameObject player)
+    {
+        if (player.GetComponent<healthController>().isDead())
+        {
+            StartCoroutine(delay(3.0f));
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    IEnumerator delay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        LoadLevel("00_menu");
+    }
+
 }
